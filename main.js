1da7cb85b13f0b4bf6d0b8e157f939e6f7d5a91e
karolyim <matej.karolyi@gmail.com>
2021-04-22 22:42:24 +0200

    [FEATURE] country propagated from data attributes to the result

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ function zobrazKalorie(udalost) {
 	let obrazek = udalost.target;
 
 	let kalorie = obrazek.dataset.kalorie;
+	let zeme = obrazek.dataset.zemePuvodu;
 
-	document.querySelector('#vysledek').textContent = 'Snědla jsi ' + kalorie + ' kcal/100g.';
+	document.querySelector('#vysledek').textContent = 'Snědla jsi ' + kalorie + ' kcal/100g a země původu je ' + zeme + '.';
 }
