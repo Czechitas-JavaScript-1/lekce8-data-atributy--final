f5898c1671be5cfe350d7f9bed1f49e1e6564c81
karolyim <matej.karolyi@gmail.com>
2021-04-22 22:39:42 +0200

    [FEATURE] calories info from data attributes propagated to the result

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-/*
-Doplň k obrázkům v HTML data atribut data-kalorie
-a jako hodnotu do něj dej číslo, představující kalorickou
-hodnotu potraviny na obrázku (číslo si klidně vymysli).
+const obrazky = document.querySelectorAll('img');
 
-Zařiď, aby se při kliknutí na obrázek volala funkce,
-která vypíše kalorickou hodnotu potraviny.
+obrazky.forEach((obrazek) => {
+	obrazek.addEventListener('click', function(event){
+		zobrazKalorie(event);
+	});
+});
 
-BONUS: počítej a zobrazuj celkový počet sněděných kalorií.
-Tj. při klikání na obrázky se postupně sčítají kalorie.
-*/
+function zobrazKalorie(udalost) {
+	let obrazek = udalost.target;
+
+	let kalorie = obrazek.dataset.kalorie;
+
+	document.querySelector('#vysledek').textContent = 'Snědla jsi ' + kalorie + ' kcal/100g.';
+}
