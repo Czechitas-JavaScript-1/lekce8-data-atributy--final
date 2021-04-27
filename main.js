cf269dff902bca5f541355b36ab1bf1916e55c55
karolyim <matej.karolyi@gmail.com>
2021-04-27 20:05:02 +0200

    [FEATURE] bonus: sum of calories

--- main.js
+++ main.js
@@ -10,7 +10,10 @@ function zobrazKalorie(udalost) {
 	let kalorie = obrazek.dataset.kalorie;
 	let zeme = obrazek.dataset.zemePuvodu;
 
+	let soucetKaloriiElement = document.querySelector('#soucet-kalorii')
+
 	document.querySelector('#vysledek').textContent = 'Snědla jsi ' + kalorie + ' kcal/100g a země původu je ' + zeme + '.';
+	soucetKaloriiElement.textContent = parseInt(soucetKaloriiElement.textContent) + parseInt(kalorie);
 
 	let snedeno = document.querySelector('#snedeno');
 	obrazek.removeEventListener('click', zobrazKalorie);
